44d37ff844c41f0456c00a8c05b9394f4446df07
Kane <gyovai@avmet.com>
2014-08-28 10:29:54 -0400

    Actors added. Time reduced.

--- src/Movie.java
+++ src/Movie.java
@@ -8,7 +8,7 @@ public class Movie {
     private Director director;
     private List<Actor> actors = new ArrayList<Actor>();
     private Script script;
-    private final int NUM_ACTORS = 4;
+    private final int NUM_ACTORS = 5;
 
     public Movie() {
         script = new Script();
@@ -21,7 +21,7 @@ public class Movie {
             for (Actor actor : actors) {
                 actor.reciteLine(i);
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(200);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
